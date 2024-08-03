31069c2afaa46bd53f872a336069758ef01b9b63
Gary Li <garylisuper@gmail.com>
2024-08-04 04:39:32 +1000

    update backend

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "my-ndf-terraform-proj-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
